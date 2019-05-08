5857b01a32ac8fc94f34e7d67e5bdacdec793505
https://github.com/YAZ444 <>
2019-05-08 10:04:26 -0400

    This is the first commit

---
+++ Alswaeer Java Project.js
@@ -0,0 +1,9 @@
+Public static void main(String[] arge) {
+// TODO code application logic here
+Alswaeer ();
+        }
+
+Public static void Alswaeer () {
+System.out.printIn ("Yaz Alswaeer!!");
+        }
+
